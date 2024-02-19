870e25f7c0c6ecce002208d932100936a841840a
Jan Umprecht <umprejan@office365.zcu.cz>
2024-02-19 14:41:42 +0100

    S2

---
+++ S2-obdelniky/S2-obdelniky/Classes/Obdelnik.cs
@@ -0,0 +1,33 @@
+﻿namespace Classes
+{
+    public class Obdelnik
+    {
+        public int StranaA { get; set; }
+        public int StranaB { get; set; }
+        public int Obvod {  get; set; }
+        public int Obsah {  get; set; }
+        public double Uhlopricka { get; set; }
+
+        public void SpoctiObvod()
+        {
+            Obvod = 2 * (StranaA + StranaB);
+        }
+
+        public void SpoctiObsah()
+        {
+            Obsah = StranaA * StranaB;
+        }
+
+        public void SpoctiUhlopricku()
+        {
+            Uhlopricka = Math.Sqrt(Math.Pow(Uhlopricka, 2) + Math.Pow(Uhlopricka, 2));
+        }
+
+        public void Vypocet()
+        {
+            SpoctiObvod();
+            SpoctiObsah();
+            SpoctiUhlopricku();
+        }
+}
+}
